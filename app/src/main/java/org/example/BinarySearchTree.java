173f0c36f79c45a6646b7093c13b90e97574f175
roodey145 <64523090+roodey145@users.noreply.github.com>
2025-11-17 20:05:05 +0100

    interface created

---
+++ app/src/main/java/org/example/BinarySearchTree.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface BinarySearchTree<T extends Comparable<T>> {
+    public T pred(T x);
+}
---
+++ app/src/main/java/org/example/SortedArrayBinarySearch.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class SortedArrayBinarySearch<T extends Comparable<T>> implements BinarySearchTree<T> {
+    private final T[] array;
+    
+    
+
+    @Override
+    public T pred(T x) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pred'");
+    }
+    
+}
