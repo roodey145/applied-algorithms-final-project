3db5f11720bbf0bc9c4e13ca092fa15f54032a39
roodey145 <64523090+roodey145@users.noreply.github.com>
2025-12-25 00:59:31 +0100

    RankSelect using hash map
    The old implementation of RankSelectLookUp. This implementation uses a hashmap which leads two time complexity of O(1) for both the rank and the select operations.

---
+++ part2/rank_select/app/src/main/java/rank_select/RankSelectLookupHashMap.java
@@ -0,0 +1,50 @@
+package rank_select;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * The assignment does not specify the type of the data that will be received
+ * The values are zeros and ones, thus, we could reduce the required space
+ * a little bit by using boolean array.
+ * 
+ * The following is a basic implementation that could be improved by utilizing
+ * parallelism.
+*/
+public class RankSelectLookupHashMap implements RankSelectInterface {
+    private final int[] rank;
+    private final Map<Integer, Integer> rankIndexMap; // Allows for constant select, space complexity O(n)
+
+    public RankSelectLookupHashMap(boolean[] data) {
+        rank = new int[data.length];
+        rankIndexMap = new HashMap<>();
+        int prevRank = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i]) {
+                // New rank detected
+                prevRank++; // Absorb the rank
+
+                // Register the rank in the map
+                rankIndexMap.put(prevRank, i); // Map rank to first index with that rank
+            }
+            rank[i] = prevRank;
+        }
+
+    }
+
+    @Override
+    public int rank(int index) throws Exception {
+        return rank[index]; // Can be used in parallel
+    }
+
+    @Override
+    public int select(int rank) throws Exception {
+        // Check if the required rank exist
+        if (!rankIndexMap.containsKey(rank)) {
+            throw new IndexOutOfBoundsException(
+                    "The rank " + rank + " does not exists! ");
+        }
+
+        return rankIndexMap.get(rank);
+    }
+}
