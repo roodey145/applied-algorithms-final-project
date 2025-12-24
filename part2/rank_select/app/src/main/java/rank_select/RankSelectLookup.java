6b0c6320fbbf53705713afb1ede058e6da0a48a9
roodey145 <64523090+roodey145@users.noreply.github.com>
2025-12-25 00:58:10 +0100

    RankSelectLookup Corrrected
    The description says that we have to use only one array of size n. In the previous implementation I used an array of size n as well as a hashmap which is not what is required, therefore I changed it.

--- part2/rank_select/app/src/main/java/rank_select/RankSelectLookup.java
+++ part2/rank_select/app/src/main/java/rank_select/RankSelectLookup.java
@@ -13,19 +13,15 @@ import java.util.Map;
 */
 public class RankSelectLookup implements RankSelectInterface {
     private final int[] rank;
-    private final Map<Integer, Integer> rankIndexMap; // Allows for constant select, space complexity O(n)
+    // private final Map<Integer, Integer> rankIndexMap; // Allows for constant select, space complexity O(n)
 
     public RankSelectLookup(boolean[] data) {
         rank = new int[data.length];
-        rankIndexMap = new HashMap<>();
         int prevRank = 0;
         for (int i = 0; i < data.length; i++) {
             if (data[i]) {
                 // New rank detected
                 prevRank++; // Absorb the rank
-
-                // Register the rank in the map
-                rankIndexMap.put(prevRank, i); // Map rank to first index with that rank
             }
             rank[i] = prevRank;
         }
@@ -39,12 +35,63 @@ public class RankSelectLookup implements RankSelectInterface {
 
     @Override
     public int select(int rank) throws Exception {
-        // Check if the required rank exist
-        if (!rankIndexMap.containsKey(rank)) {
-            throw new IndexOutOfBoundsException(
-                    "The rank " + rank + " does not exists! ");
-        }
+        return select(rank, 0, this.rank.length - 1, 0, this.rank.length - 1);
+    }
 
-        return rankIndexMap.get(rank);
+    
+
+    private int select(int target, int low, int high, int prevLowAnchor, int prevLargeAnchor) {
+        if(low > high) return -1;
+        int mid = low + (high - low) / 2;
+        
+        if(rank[mid] == target) {
+            if((mid > low && rank[mid - 1] != target) || mid == low) {
+                // Mid is the index of the first element that has this rank
+                return mid;
+            }
+            return select(target, low, (mid - 1), prevLowAnchor, mid);
+        } 
+        else if (rank[mid] > target) {
+            return select(target, low, (mid - 1), prevLowAnchor, mid);
+        } else { // The rank of this element is less than the target
+            return select(target, mid + 1, high, mid, prevLargeAnchor);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        boolean[] ranksData = new boolean[] {
+            false, // 0 index 1
+            false, // 0 
+            true, // 1 index 2
+            false, // 1
+            false, // 1
+            true, // 2 index 5
+            true, // 3 index 6
+            true, // 4 index 7
+            false, // 4 
+            false, // 4
+        };
+        RankSelectLookup rs = new RankSelectLookup(ranksData);
+
+        System.out.println("========RANKS========");
+        System.out.println((rs.rank(0) == 0));
+        System.out.println(rs.rank(1) == 0);
+        System.out.println(rs.rank(2) == 1);
+        System.out.println(rs.rank(3) == 1);
+        System.out.println(rs.rank(4) == 1);
+        System.out.println(rs.rank(5) == 2);
+        System.out.println(rs.rank(6) == 3);
+        System.out.println(rs.rank(7) == 4);
+        System.out.println(rs.rank(8) == 4);
+        System.out.println(rs.rank(9) == 4);
+        System.out.println("=====================\n");
+
+        System.out.println("========SELECT=======");
+        System.out.println((rs.select(0) == 0));
+        System.out.println((rs.select(1) == 2));
+        System.out.println((rs.select(2) == 5));
+        System.out.println((rs.select(3) == 6));
+        System.out.println((rs.select(4) == 7));
+        System.out.println("=====================\n");
     }
 }
