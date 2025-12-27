2ca1a1ea52ee5bda79ae79761a33967ac85318f0
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-12-27 21:18:30 +0100

    Experiment file also reflects the new changes

--- part1/skewed_binary_trees/app/src/main/java/org/example/Experiments.java
+++ part1/skewed_binary_trees/app/src/main/java/org/example/Experiments.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Experiments {
     public static void main(String[] args) {
         // List<Integer> list = new ArrayList<>();
         // list.add(1);
 
-        Integer[] array = new Integer[] { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        int[] array = new int[] { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                12, 13, 14, 15 };
         OtherArrayBinarySearch sortedArray = new OtherArrayBinarySearch(array, 0.7f);
 
         // sortedArray.printTree();
