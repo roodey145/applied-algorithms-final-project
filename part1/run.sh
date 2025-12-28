0d58eb88bf6d18cfca83b8a89efb63491680a9bf
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-12-28 22:01:18 +0100

    Create run.sh file that automatically runs tests and experiments

---
+++ part1/run.sh
@@ -0,0 +1,13 @@
+cd skewed_binary_trees
+echo "........Running Gradle build, automatically runs the tests........"
+gradle build
+cd ../input
+echo "........Generating the input files needed for the experiments........"
+python3 createInput.py
+cd ..
+echo "........Running the experiment, this will take a while........"
+python3 experiment.py
+cd plots/
+echo "Generating the plots from the results of the experiments
+open *.pdf
+cd ..
