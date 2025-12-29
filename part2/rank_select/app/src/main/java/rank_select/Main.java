83e425192f151cc6c2aaf68568fb42bb015b48ef
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-12-29 20:36:03 +0100

    Change App to Main and create jar file

--- part2/rank_select/app/src/main/java/rank_select/Main.java
+++ part2/rank_select/app/src/main/java/rank_select/Main.java
@@ -3,12 +3,9 @@
  */
 package rank_select;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello World");
     }
 }
